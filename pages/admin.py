5c0b1e33b5487f80102302f18841920c523fe039
n-serrette <serrette.nicola@gmail.com>
2019-02-01 18:15:09 +0100

    Chunks admin collapsable field set per language

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from translated_fields import TranslatedFieldAdmin, to_attribute
+import re
 from pages.models import Chunk
 
 
@@ -8,11 +10,18 @@ from pages.models import Chunk
 class ChunkAdmin(TranslatedFieldAdmin, admin.ModelAdmin):
     list_display = ('key', "description")
 
-    # Pack question and answer fields into their own fieldsets:
+    base_fields = ['description', 'content']
     fieldsets = [
-        (_("description"), {"fields": Chunk.description.fields}),
-        (_("content"), {"fields": Chunk.content.fields}),
+        (_("Globals"), {"fields": ["key"]}),
     ]
+    for i, language in enumerate(settings.LANGUAGES):
+        ft = []
+        for field in base_fields:
+            ft.append(re.sub(r"[^a-z0-9_]+", "_", ("%s_%s" % (field, language[0])).lower()))
+        if i > 0:
+            fieldsets.append((_(language[1]), {"fields" : ft, 'classes': ['collapse']}))
+        else:
+            fieldsets.append((_(language[1]), {"fields" : ft}))
 
     def get_ordering(self, request):
         return [to_attribute("description")]
