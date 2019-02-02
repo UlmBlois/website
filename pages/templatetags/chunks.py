3a0fab9f60053d806a590cd2e4127a989d0de682
n-serrette <serrette.nicola@gmail.com>
2019-02-02 12:48:56 +0100

    add evalutate template tag, allow evalutation of tags inside a textfield

--- pages/templatetags/chunks.py
+++ pages/templatetags/chunks.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template import Context, Template
 from pages.models import Chunk
 
 register = template.Library()
@@ -7,3 +8,31 @@ register = template.Library()
 @register.simple_tag
 def get_chunk(key):
     return Chunk.objects.get(key=key)
+
+
+class RenderCustom(template.Node):
+
+    @classmethod
+    def handle_token(cls, parser, token):
+        tokens = token.split_contents()
+
+        field = tokens[1]
+
+        return cls(parser.compile_filter(field))
+
+    def __init__(self, field):
+        self.field = field
+
+    def render(self, context):
+        render_field = self.field.resolve(context)
+
+        render_template = Template(render_field)
+
+        rendered = render_template.render(Context())
+
+        return rendered
+
+
+@register.tag
+def evaluate(parser, token):
+    return RenderCustom.handle_token(parser, token)
