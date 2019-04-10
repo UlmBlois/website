c33943b4b653c25002caddc47891acac91124955
n-serrette <serrette.nicola@gmail.com>
2019-04-10 19:04:49 +0200

    confirmation reminder

--- core/management/commands/send_reservation_confirmation_request.py
+++ core/management/commands/send_reservation_confirmation_request.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.utils.translation import gettext_lazy as _
+from django.template.loader import render_to_string
+from django.core.mail import send_mass_mail
 from datetime import date
 from meeting.models import Reservation, Meeting
 
@@ -8,16 +10,21 @@ class Command(BaseCommand):
     help = _('Send an email asking users to confirm their reservation (active '
              'meeting only). The email will be sent only if on the specified '
              'date in the meeting settings.')
-    email_template = ''
+    email_template = 'reservation_confirmation_request.html'
 
     def handle(self, *args, **kwargs):
         meeting = Meeting.objects.active()
+        subject = _("Confirm your reservation")
+        from_email = 'noreply@salon-ulm-blois.fr'
+        message = render_to_string(self.email_template)
+        email_pack = []
         if meeting.confirmation_reminder_date == date.today():
             unconfirmed_res = Reservation.objects.unconfirmed_actives()
             email_list = [x.ulm.pilot.user.email for x in unconfirmed_res
                           if x.ulm is not None and x.ulm.pilot is not None]
             for email in email_list:
-                self.stdout.write(email)  # TODO send email
-
+                email_pack.append((subject, message, from_email, [email]))
+                # self.stdout.write(email)  # TODO send email
+            send_mass_mail(email_pack, fail_silently=False)
         else:
             self.stdout.write("Nothing to do")
