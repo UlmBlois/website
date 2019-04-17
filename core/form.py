6996a4996954aa01032c82e782bb6c84ed323c4a
n-serrette <serrette.nicola@gmail.com>
2019-04-17 19:59:43 +0200

    crispy-form and other form refactoring

--- core/form.py
+++ core/form.py
@@ -1,5 +1,11 @@
+# Django
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext_lazy as _
+# Third party
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column, Field
+# Owned
 from django.contrib.auth.models import User
 
 
@@ -13,3 +19,23 @@ class SignUpForm(UserCreationForm):
         fields = (
             'username', 'first_name', 'last_name',
             'email', 'password1', 'password2', )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Field('username', css_class='form-group'),
+            Row(
+                Column('first_name', css_class='form-group col-md-6 mb-0'),
+                Column('last_name', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            Field('email', css_class='form-group'),
+            Row(
+                Column('password1', css_class='form-group col-md-6 mb-0'),
+                Column('password2', css_class='from-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            Submit('submit', _('Register')),
+        )
