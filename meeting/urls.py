e6f289d691c9f21760d62e6f2934c1f881a9e470
n-serrette <serrette.nicola@gmail.com>
2019-07-19 18:34:16 +0200

    add some images

--- meeting/urls.py
+++ meeting/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-# from meeting import filters
+from meeting import filters
 
 
 urlpatterns = [
@@ -50,22 +50,22 @@ urlpatterns += [
          name="staff_update_pilot"),
     path('staff/pilot/<int:pk>/overview/', views.PilotOverview.as_view(),
          name='pilot_overview'),
-    # path('staff/ulm/', views.FilteredULMList.as_view(
-    #     filterset_class=filters.ULMFilter,
-    #     template_name='staff_ulm_list.html'),
-    #     name='staff_ulm_list'),
-    # path('staff/pilot/', views.FilteredPilotList.as_view(
-    #     filterset_class=filters.PilotFilter,
-    #     template_name='staff_pilot_list.html'),
-    #     name='staff_pilot_list'),
-    # path('staff/fuel/', views.FilteredReservationList.as_view(
-    #      filterset_class=filters.ReservationFilter,
-    #      template_name='staff_fuel_reservation_list.html'),
-    #      name="staff_fuel_res_list"),
-    # path('staff/reservation/', views.FilteredReservationList.as_view(
-    #     filterset_class=filters.ReservationFilter,
-    #     template_name='staff_reservation_list.html'),
-    #     name='staff_reservation_list'),
+    path('staff/ulm/', views.FilteredULMList.as_view(
+        filterset_class=filters.ULMFilter,
+        template_name='staff_ulm_list.html'),
+        name='staff_ulm_list'),
+    path('staff/pilot/', views.FilteredPilotList.as_view(
+        filterset_class=filters.PilotFilter,
+        template_name='staff_pilot_list.html'),
+        name='staff_pilot_list'),
+    path('staff/fuel/', views.FilteredReservationList.as_view(
+         filterset_class=filters.ReservationFilter,
+         template_name='staff_fuel_reservation_list.html'),
+         name="staff_fuel_res_list"),
+    path('staff/reservation/', views.FilteredReservationList.as_view(
+        filterset_class=filters.ReservationFilter,
+        template_name='staff_reservation_list.html'),
+        name='staff_reservation_list'),
     path('staff/reservation/validation/<int:pk>/overview/',
          views.StaffReservationValidationDetail.as_view(),
          name='staff_reservation_overview'),
