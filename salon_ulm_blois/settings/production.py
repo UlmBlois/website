42bff23c3a26556543f57be4a5ea0421b6a41c5d
n-serrette <serrette.nicola@gmail.com>
2019-07-21 19:22:57 +0200

    up settings

--- salon_ulm_blois/settings/production.py
+++ salon_ulm_blois/settings/production.py
@@ -8,7 +8,8 @@ from pathlib import Path  # python3 only
 
 
 # OR, explicitly providing path to '.env'
-env_path = Path('/home/genos/website/salon_ulm_blois/settings/.env')
+
+env_path = os.path.join(os.path.dirname(__file__), '/.env')
 load_dotenv(dotenv_path=env_path)
 
 
