b0829396bae1cb956c161fd4be748333a555b11f
n-serrette <serrette.nicola@gmail.com>
2020-02-03 07:56:09 +0100

    update dependencies

--- meeting/tests/test_charts.py
+++ meeting/tests/test_charts.py
@@ -6,11 +6,13 @@ from datetime import date, datetime
 from meeting.tests.utils import (create_meeting, create_time_slot,
                                  PermissionRequiredTestCase, create_user)
 
+
 # TODO FINISH
 class MeetingStatsView(PermissionRequiredTestCase, TestCase):
     url = '/meeting/chart/meeting'
     url_name = 'meeting_stat'
     template_name = 'meeting_statistic.html'
+    permission_required = ['reservation_validation']
 
     @classmethod
     def setUpTestData(cls):
